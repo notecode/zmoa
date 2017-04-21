f2c6d495984332c5668f8b243d94dbc0ef908762
songerwei <songerwei@xxtao.com>
2017-04-21 11:37:23 +0800

    1. del useless files; 2. admin/schedule.html, init

---
+++ src/imodules/www/admin/schedule/main.js
@@ -0,0 +1,19 @@
+define(function() {
+    var Module = (function() {
+        var baseIModules = project.baseIModules;
+        var CON = function(dom) {
+            baseIModules.BaseIModule.call(this, dom);
+        };
+        potato.createClass(CON, baseIModules.BaseIModule);
+
+        CON.prototype.x = function() {
+        }
+
+        CON.prototype._ievent_ = function(data, target, hit) {
+        }
+
+        return CON;
+    })();
+
+    return Module;
+})
