4cc0bfde5b6bf22cc6ab6e66ca775762e7e31b4b
gaoyue <gaoyueyox@163.com>
2017-04-24 15:13:42 +0800

    修改字体

--- src/imodules/www/admin/serviceProcess/main.js
+++ src/imodules/www/admin/serviceProcess/main.js
@@ -1,12 +1,22 @@
-define(function () {
-    var baseIModules = project.baseIModules;
-    var Module = (function () {
-        var CON = function (dom) {
+define(function() {
+    var Module = (function() {
+        var baseIModules = project.baseIModules;
+        var CON = function(dom) {
             baseIModules.BaseIModule.call(this, dom);
-            
-            
+            this.getInfo();
         };
         potato.createClass(CON, baseIModules.BaseIModule);
+
+        CON.prototype.getInfo = function() {
+            api_ajax('project/statistic', {
+                succ: function(json) {
+                    
+                },
+                fail: function(json) {
+
+                }
+            });
+        }
 
         return CON;
     })();
