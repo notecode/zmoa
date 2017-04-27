fa3d25b25e5439bca098037830febc1fabf8c0ee
songerwei <songerwei@xxtao.com>
2017-04-27 19:25:01 +0800

    login

--- src/imodules/www/login/main.js
+++ src/imodules/www/login/main.js
@@ -1,16 +1,45 @@
-define(function () {
-    var baseIModules = project.baseIModules;
-    var Module = (function () {
-        var CON = function (dom) {
-            baseIModules.BaseIPage.call(this,dom);
-            this.find('#InquiryMap').removeHide();
+define(function() {
+    var Module = (function() {
+		var baseIModules = project.baseIModules;
+        var CON = function(dom) {
+            baseIModules.BaseIModule.call(this, dom);
         };
-        potato.createClass(CON, baseIModules.BaseIPage);
-        
+        potato.createClass(CON, baseIModules.BaseIModule);
+		
+        CON.prototype._ievent_login = function() {
+            this.find('.error-tips').hide();
 
+            var acc = this.find('#account').val();
+            var pwd = this.find('#password').val();
+
+            if (0 == acc.length) {
+                this.showTips('请您输入账号');
+            } else if (0 == pwd.length) {
+                this.showTips('请您输入密码');
+            } else {
+                var data = {
+                    userName: acc,
+                    password: pwd
+                };
+                var _this = this;
+                api_ajax_post('user/login', data, {
+                    succ: function(json) {
+                        tlog('login succ');
+                    },
+                    fail: function(json) {
+                        _this.showTips(json.errmsg);
+                    }
+                });
+            }
+        }
+
+        CON.prototype.showTips = function(tips) {
+            this.find('.error-data').text(tips);
+            this.find('.error-tips').show();
+        }
+        
         return CON;
     })();
 
     return Module;
-});
-
+})
