4938bd35b150de81bd4669791699eac1e7d66a45
songerwei <songerwei@xxtao.com>
2017-04-30 22:32:28 +0800

    m sch, ok

--- src/iscripts/global/test/mock/api-4-m-sch.js
+++ src/iscripts/global/test/mock/api-4-m-sch.js
@@ -1,20 +1,24 @@
 define(function() {
-    return [
-        {
-            start_date: "2017-04-03",
-            end_date: "2017-04-06"
-        },
-        {
-            start_date: "2017-04-20",
-            end_date: "2017-04-25"
-        },
-        {
-            start_date: "2017-04-28",
-            end_date: "2017-05-03"
-        },
-        {
-            start_date: "2017-05-05",
-            end_date: "2017-05-08"
-        },
-    ]
+    return {
+        min_date: "2017-04-03",
+        max_date: "2017-05-08",
+        projects: [
+            {
+                start_date: "2017-04-03",
+                end_date: "2017-04-06"
+            },
+            {
+                start_date: "2017-04-20",
+                end_date: "2017-04-25"
+            },
+            {
+                start_date: "2017-04-28",
+                end_date: "2017-05-03"
+            },
+            {
+                start_date: "2017-05-05",
+                end_date: "2017-05-08"
+            },
+        ]
+    }
 })
