1003a77bce9608cb491f666794dca62a7a5bcbbc
songerwei <songerwei@xxtao.com>
2017-04-30 20:27:34 +0800

    bottom btn: style and event

--- src/imodules/m/engineer/editRepair/main.js
+++ src/imodules/m/engineer/editRepair/main.js
@@ -9,33 +9,29 @@ define(["/global/iscripts/test/mock/api-4-project-detail.js"], function(mock) {
             project.getIModule('imodule://sparesMD', null, function(mod) {
                 mod.render(proj);
             });
-
-            this.bindSubmit();
         };
         potato.createClass(CON, baseIModules.BaseIModule);
 		
-		CON.prototype.bindSubmit = function() {
+		CON.prototype._ievent_save = function() {
             var _this = this;
-            this.find('.paiqi-btn').click(function() {
-                var log = _this.find('#repair-log').val();
-                if (log.length > 0) {
-                    var data = {
-                        projectId: qs_proj(),
-                        comment: log 
-                    };
+            var log = this.find('#repair-log').val();
+            if (log.length > 0) {
+                var data = {
+                    projectId: qs_proj(),
+                    comment: log 
+                };
 
-                    api_ajax_post('project/add_comment_to_project', data, {
-                        succ: function(json) {
-                            alert('保存成功');
-                        },
-                        fail: function(json) {
-                            alert(json.errmsg);
-                        }
-                    });
-                } else {
-                    alert('请输入维修记录后，再保存');
-                }
-            });
+                api_ajax_post('project/add_comment_to_project', data, {
+                    succ: function(json) {
+                        alert('保存成功');
+                    },
+                    fail: function(json) {
+                        alert(json.errmsg);
+                    }
+                });
+            } else {
+                alert('请输入维修记录后，再保存');
+            }
 		}
         
         return CON;
