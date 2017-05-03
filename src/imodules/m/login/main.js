c481cf48d81ec2c4b6cb4f457649a7801510a2d3
songerwei <songerwei@xxtao.com>
2017-05-04 00:04:12 +0800

    sms passcode login, ok

--- src/imodules/m/login/main.js
+++ src/imodules/m/login/main.js
@@ -3,12 +3,24 @@ define(function() {
 		var baseIModules = project.baseIModules;
         var CON = function(dom) {
             baseIModules.BaseIModule.call(this, dom);
+            this.via_pwd = (qs('via') != 'phone');  // 只要不是?pwd=phone，均以用户名+密码方式登录
+
+            if (this.via_pwd) {
+                this.find('.via-user-pwd').removeClass('hide');
+            } else {
+                this.find('.via-phone').removeClass('hide');
+            }
 
             var _this = this;
             // 校验用户名和密码
             $('.js-check-field').keydown(debounce(function (e) {
                 console.log('action');
-                _this.fieldCheck();
+
+                if (_this.via_pwd) {
+                    _this.fieldCheckForPwd();
+                } else {
+                    _this.fieldCheckForPhone();
+                }
             }, 300))
 
             // 防止Enter键触发
@@ -17,8 +29,9 @@ define(function() {
             });
         };
         potato.createClass(CON, baseIModules.BaseIModule);
+
 		// 校验方法
-        CON.prototype.fieldCheck = function() {
+        CON.prototype.fieldCheckForPwd = function() {
             var $name = $.trim($(this._els.LName).val());
             var $password = $.trim($(this._els.LPassword).val());
             var $loginButton = $(this._els.LButton);
@@ -28,9 +41,77 @@ define(function() {
                 $loginButton.attr('disabled', true);
             }
         }
+
+        CON.prototype.fieldCheckForPhone = function() {
+            var phone = $(this._els.phone).val();
+            var pass = $(this._els.passcode).val();
+            var getPass = $(this._els.getcode); 
+            var login = $(this._els.LButton);
+
+            // 这几个数字，是预期“快输够”的时候，可用
+            if (phone.length > 9) {
+                getPass.attr('disabled', false);
+
+                if (pass.length >= 3) {
+                    login.attr('disabled', false);
+                }
+            }
+        }
+
+        CON.prototype._ievent_getPasscode = function(data, target, hit) {
+            tlog('you clicked get passcode');
+
+            var phone = $(this._els.phone).val();
+            if (!is_phone_valid(phone)) {
+                project.tip('请输入有效手机号', 'fail', '', true);
+                return false;
+            }
+
+            var max_cnt = 60;
+            //var max_cnt = 20;
+            var getPass = $(this._els.getcode); 
+            getPass.attr('disabled', true);
+            var cnt = max_cnt;
+            var timerId = setInterval(function() {
+                var txt = --cnt + 's后重新获取';
+                getPass.text(txt);
+            }, 1000);
+
+            setTimeout(function() {
+                tlog(max_cnt + 's passed, enable button again');
+                getPass.attr('disabled', false);
+                getPass.text('获取验证码');
+                clearInterval(timerId);
+            }, max_cnt * 1000);
+
+            api_ajax_post('user/mobile_send_msg', {mobile: phone}, {
+                succ: function(json) {
+                    tlog('send passcode succ');
+                },
+                fail: function(json) {
+                    tlog(json.errmsg);
+                }
+            });
+        }
+
         CON.prototype._ievent_login = function() {
-            var user = $(this._els.LForm).serializeJSON();
-            api_ajax_post('user/login', user, {
+            var uri = '';
+            var data = null;
+            if (this.via_pwd) {
+                uri = 'user/login';
+                data = {
+                    userName: $.trim($(this._els.LName).val()),
+                    password: $.trim($(this._els.LPassword).val())
+                }
+            } else {
+                uri = 'user/mobile_login';
+                data = {
+                    mobile: $(this._els.phone).val(),
+                    msgCode: $(this._els.passcode).val()
+                };
+            }
+
+            api_ajax_post(uri, data, {
                 succ: function(json) {
                     tlog('login succ');
 
