b31baa7215b7d514122c5f0e51cc08daed7c7de0
gaoyue <gaoyueyox@163.com>
2017-05-16 17:18:23 +0800

    增加销售员项目列表页面

---
+++ src/imodules/m/sales/requirementsMD/main.js
@@ -0,0 +1,74 @@
+define(["/global/iscripts/libs/time/moment.js"], function(moment) {
+    var Module = (function() {
+		var baseIModules = project.baseIModules;
+        var CON = function(dom) {
+            baseIModules.BaseIModule.call(this, dom);
+            this.tpl = this._els.tpl[0].text;
+        };
+        potato.createClass(CON, baseIModules.BaseIModule);
+
+        var APPLY_ENVIRONMENT = {
+            1: '户外',
+            2: '室内',
+            3: '半户外',
+        };        
+        var SCREEN_COLOR = {
+            1: '双色',
+            2: '单色',
+            3: '全彩',
+        };
+
+		CON.prototype.render = function(proj) {
+            var worker = proj.service_user || {};
+            proj.main_img = proj_img_url(proj.main_img);
+            var dom = Mustache.render(this.tpl, {
+                proj: proj,
+                fn: {
+                    apply_environment: function() {
+                        return APPLY_ENVIRONMENT[proj.apply_environment];
+                    },
+                    screen_color: function() {
+                        return SCREEN_COLOR[proj.screen_color];
+                    },
+                    hide_main_img: function() {
+                        var img = proj.main_img;
+                        return (img && img.length > 0) ? '' : 'hide';
+                    },
+                    sched: function() {
+                        var start = worker.start_date;
+                        return (start && start.length > 0) ? 'show' : 'hide';
+                    },
+                    sch_start: function() {
+                        var start = worker.start_date;
+                        return start ? moment(start).format('M月DD日') : '';
+                    },
+                    sch_end: function() {
+                        var end = worker.end_date;
+                        return end ? moment(end).format('M月DD日') : '';
+                    }
+                }
+            });
+            this.find('.details').append(dom);
+
+            this.bindFoldEvents();
+		}
+		
+		CON.prototype.bindFoldEvents = function() {
+            var fold = this.find('.fold-table');
+            var unfold = this.find('.unfold-table');
+            var table = this.find('.info-table');
+
+            var tog = function() {
+                fold.toggle();
+                unfold.toggle();
+                table.toggle();
+            };
+            fold.click(tog);
+            unfold.click(tog);
+		}
+        
+        return CON;
+    })();
+
+    return Module;
+})
