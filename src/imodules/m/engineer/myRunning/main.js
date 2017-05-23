25fb399c9a23a1dd8303e64888a3cf5c053bdb6b
gaoyue <gaoyueyox@163.com>
2017-05-23 12:44:35 +0800

    增加工程商我执行中的项目的功能

--- src/imodules/m/engineer/myRunning/main.js
+++ src/imodules/m/engineer/myRunning/main.js
@@ -1,64 +1,30 @@
-define(["/global/iscripts/test/mock/api-4-project-detail.js"], function(mock) {
+define(["/global/iscripts/libs/time/moment.js"], function(moment) {
     var Module = (function() {
 		var baseIModules = project.baseIModules;
         var CON = function(dom) {
             baseIModules.BaseIModule.call(this, dom);
             this.tpl = this._els.tpl[0].text;
-
-            var doRender = function(proj) {
-                project.getIModule('imodule://sparesMD', null, function(mod) {
-                    mod.render(proj);
-                });
-            }
-
-            if (1 == qs('test')) {
-                doRender(mock.project_info);
-            } else {
-                api_ajax('project/detail/' + qs_proj(), {
-                    succ: function(json) {
-                        doRender(json.project_info);
-                    }
-                });
-            }
+            this.getList();
         };
         potato.createClass(CON, baseIModules.BaseIModule);
 		
-		CON.prototype._ievent_save = function() {
+		CON.prototype.getList = function() {
             var _this = this;
-            var log = this.find('#repair-log').val();
-            if (log.length > 0) {
-                var data = {
-                    projectId: qs_proj(),
-                    comment: filterCR(log)
-                };
-
-                api_ajax_post('project/add_comment_to_project', data, {
-                    succ: function(json) {
-                        _this.transferToComplete();
-                    },
-                    fail: function(json) {
-                        alert(json.errmsg);
-                    }
-                });
-            } else {
-                alert('请输入维修记录后，再保存');
-            }
-		}
-
-        CON.prototype.transferToComplete = function() {
-            var data = {
-                projectId: qs_proj(),
-                status: 4  // 已完成，待回访
-            };
-            api_ajax_post('project/transfer_status', data, {
+            api_ajax('project/engineer_going_projects', {
                 succ: function(json) {
-                    project.tip('保存成功', 'succ', '');
-                    setTimeout(function() {
-                        location.href = '/project/detail.html?project=' + qs_proj();
-                    }, 3000);
+                    for (var i=0; i<json.list.length; i++){
+                        if(json.list[i].start_date) {
+                            json.list[i].start_date = moment(json.list[i].start_date).format('M月DD日');
+                        }
+                        if(json.list[i].end_date) {
+                            json.list[i].end_date = moment(json.list[i].end_date).format('M月DD日');
+                        }
+                    }
+                    var dom = Mustache.render(_this.tpl, json); 
+                    $(_this._els.proRunning).html(dom);
                 },
                 fail: function(json) {
-                    alert(json.errmsg);
+
                 }
             });
         }
