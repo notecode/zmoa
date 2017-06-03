6af39c00680773db3ce080f5c5b9a54a58da27c8
SongErwei <songerwei@xxtao.com>
2017-06-03 21:20:32 +0800

    点击：完成（配件维修）

--- src/imodules/m/engineer/myRunning/main.js
+++ src/imodules/m/engineer/myRunning/main.js
@@ -4,7 +4,8 @@ define(["/global/iscripts/libs/time/moment.js"], function(moment) {
         var CON = function(dom) {
             baseIModules.BaseIModule.call(this, dom);
             this.tpl = this._els.tpl[0].text;
-            this.turnFor();
+            this.popupTpl = this._els.tpl[1].text;
+
             var _this = this;
             project.events.addListener('login.ensured', function(event) {
                 _this.getList();
@@ -29,6 +30,7 @@ define(["/global/iscripts/libs/time/moment.js"], function(moment) {
 
                     var dom = Mustache.render(_this.tpl, json); 
                     $(_this._els.proRunning).html(dom);
+                    _this.bindClick();
                 },
                 fail: function(json) {
                     alert(json.errmsg);
@@ -36,17 +38,59 @@ define(["/global/iscripts/libs/time/moment.js"], function(moment) {
             });
         }
 
-        CON.prototype.turnFor = function(){
+        CON.prototype.bindClick = function(){
             var _this = this;
-            $(this._els.proRunning).on('click','.list-implement',function(e){
-                var pid = $(this).data().id;
-                location.href='/project/detail.html?projectId=' + pid;
+            $(this._els.proRunning).on('click', '.project', function(e){
+                var projId = $(this).data().id;
+                if (0 == $(this).data().type) {
+                    // 厂外项目
+                    location.href='/project/detail.html?projectId=' + projId;
+                } else {
+                    _this.popupPartsPane(projId);
+                }
             })
+        }
 
-            $(this._els.proRunning).on('click','.list-depot',function(e){
-                $(_this._els.rMask).removeHide();
-                $(_this._els.depotRepair).addClass('slideUp');
+        CON.prototype.popupPartsPane = function(projId) {
+            var _this = this;
+            var q = {ignore_done: 1};
+            api_ajax_with_query('project/parts_repair_status/' + projId, q, {
+                succ: function(json) {
+                    var dom = Mustache.render(_this.popupTpl, json); 
+                    $(_this._els.popup).empty().append(dom);
+                    _this.bindRepairDone(projId);
+
+                    $(_this._els.rMask).removeHide();
+                    $(_this._els.depotRepair).addClass('slideUp');
+                },
+                fail: function(json) {
+                    alert(json.errmsg);
+                }
             })
+        }
+
+        CON.prototype.bindRepairDone = function(projId) {
+            var _this = this;
+            $(_this._els.popup).off('click.forDone').on('click.forDone', '.repair_done', function() {
+                tlog('clicked');
+                var jq = $(this);
+                var data = {
+                    projectId: projId,
+                    partsId: $(this).data().id
+                };
+                api_ajax_post('project/post_parts_repair_done', data, {
+                    succ: function(json) {
+                        jq.parent().remove();
+
+                        if (0 == json.not_repaired) {
+                            location.reload();
+                        }
+                    },
+                    fail: function(json) {
+                        alert(json.errmsg);
+                    }
+                })
+            });
         }
 
         CON.prototype._ievent_closeMask = function(data, target, hit){
