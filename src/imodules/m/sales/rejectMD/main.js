1b9ef35390f749b4a3bf5d0941f7ed0ab832c900
SongErwei <songerwei@xxtao.com>
2017-06-07 17:49:19 +0800

    reject reason

--- src/imodules/m/sales/rejectMD/main.js
+++ src/imodules/m/sales/rejectMD/main.js
@@ -5,8 +5,27 @@ define(function() {
             baseIModules.BaseIModule.call(this, dom);
         };
         potato.createClass(CON, baseIModules.BaseIModule);
-
         
+        CON.prototype._ievent_reject = function() {
+            var _this = this;
+            var reas = $(this._els.reason).val();
+            if (reas.length > 0) {
+                var data = {
+                    projectId: qs_proj(),
+                    desc: filterCR(reas)
+                };
+                api_ajax_post('project/reject', data, {
+                    succ: function(json) {
+                        location.reload();
+                    },
+                    fail: function(json) {
+                        alert(json.errmsg);
+                    }
+                });
+            } else {
+                alert('请输入驳回原因');
+            }
+        }
      
         return CON;
     })();
